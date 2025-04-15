4fb9cf02fc21544918424d205fd8d5e6d8cd218b
Juan <eysless@gmail.com>
2025-04-15 20:44:29 +0200

    added some interactions

--- caronte-tools/caronte-setup/app.py
+++ caronte-tools/caronte-setup/app.py
@@ -1,8 +1,13 @@
 from caronteInteract import *
 from flask import Flask, render_template, request, jsonify
 
-app = Flask(__name__)
+password = 'e26347d0c030e16a1446bcd4989722a3d4ed3bd9f6f2f0ec762c069b88ef1eefcdf4cbdb54ed5a3499963290e6c5d008cc5c44353872f08fa1d094e7a6701c11'
+caronteip = '10.81.63.7:1111'
+caronte = Caronte(f"http://root:{password}@{caronteip}")
 
+
+app = Flask(__name__)
+global serverip
 def handle_input(user_input):
     print(f"User typed: {user_input}")
     return f"Received: {user_input}"
@@ -18,5 +23,22 @@ def process():
     result = handle_input(user_input)
     return jsonify({'result': result})
 
+@app.route('/ipProcess', methods=['POST'])
+def ipProcess():
+    data = request.get_json()
+    user_input = data.get('text')
+    result = handle_input(user_input)
+    serverip = user_input
+    return jsonify({'result': result})
+
+@app.route('/launchSetup', methods=['POST'])
+def launchSetup():
+    data = request.get_json()
+    user_input = data.get('text')
+    result = handle_input(user_input)
+    serverip = user_input
+    return jsonify({'risposta': result})
+
 if __name__ == '__main__':
     app.run(debug=True)
+
